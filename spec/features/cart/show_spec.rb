5814c193cbe87c653067d2d06a7edf16c2698234
Alice Post <alicep2322@gmail.com>
2019-10-12 22:27:27 -0600

    Remove @ from cart/show_spec where not needed
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -11,30 +11,30 @@ RSpec.describe "cart show page" do
     - a subtotal (price multiplied by quantity)
     - grand total of what everything in my cart will cost" do
 
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
-      expect(page).to have_content(@chain.name)
-      expect(page).to have_css("img[src*='#{@chain.image}']")
-      expect(page).to have_content(@chain.merchant.name)
-      expect(page).to have_content(@chain.price)
+      expect(page).to have_content(chain.name)
+      expect(page).to have_css("img[src*='#{chain.image}']")
+      expect(page).to have_content(chain.merchant.name)
+      expect(page).to have_content(chain.price)
       expect(page).to have_content("Quantity: 1")
       expect(page).to have_content("Item Subtotal: $50.00")
-      expect(page).to have_content(@shifter.name)
-      expect(page).to have_css("img[src*='#{@shifter.image}']")
-      expect(page).to have_content(@shifter.merchant.name)
-      expect(page).to have_content(@shifter.price)
+      expect(page).to have_content(shifter.name)
+      expect(page).to have_css("img[src*='#{shifter.image}']")
+      expect(page).to have_content(shifter.merchant.name)
+      expect(page).to have_content(shifter.price)
       expect(page).to have_content("Quantity: 2")
       expect(page).to have_content("Item Subtotal: $360.00")
       expect(page).to have_content("Grand Total: $410.00")
@@ -43,10 +43,10 @@ RSpec.describe "cart show page" do
 
     it "shows an empty cart when no items are added" do
 
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
       visit "/cart"
       expect(page).to have_content("Your cart is empty")
@@ -55,17 +55,17 @@ RSpec.describe "cart show page" do
     end
 
     it "has a link to empty the cart" do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @tire = @meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
@@ -82,64 +82,64 @@ RSpec.describe "cart show page" do
     end
 
     it "can remove an individual item from cart" do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
-      within "#item-#{@chain.id}" do
+      within "#item-#{chain.id}" do
         click_button "Remove Item"
       end
 
       expect(current_path).to eq('/cart')
 
       within "#cart-table" do
-        expect(page).to_not have_content(@chain.name)
-        expect(page).to have_content(@shifter.name)
+        expect(page).to_not have_content(chain.name)
+        expect(page).to have_content(shifter.name)
       end
 
       visit "/cart"
-      within "#item-#{@shifter.id}" do
+      within "#item-#{shifter.id}" do
         click_button "Remove Item"
       end
 
       expect(current_path).to eq('/cart')
 
       within "#cart-table" do
-        expect(page).to_not have_content(@shifter.name)
+        expect(page).to_not have_content(shifter.name)
       end
     end
 
     it "can increment the count of an individual item according to inventory" do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
-      within "#item-#{@chain.id}" do
+      within "#item-#{chain.id}" do
         expect(page).to have_content("Quantity: 1")
         click_button "+"
         expect(page).to have_content("Quantity: 2")
       end
       expect(current_path).to eq('/cart')
 
-      within "#item-#{@shifter.id}" do
+      within "#item-#{shifter.id}" do
         expect(page).to have_content("Quantity: 2")
         click_button "+"
         expect(page).to have_content("Quantity: 2")
@@ -149,20 +149,20 @@ RSpec.describe "cart show page" do
     end
 
     it "can decrement the count of an individual item according to inventory" do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
-      within "#item-#{@chain.id}" do
+      within "#item-#{chain.id}" do
         expect(page).to have_content("Quantity: 1")
         click_button "-"
       end
@@ -172,7 +172,7 @@ RSpec.describe "cart show page" do
 
       expect(current_path).to eq('/cart')
 
-      within "#item-#{@shifter.id}" do
+      within "#item-#{shifter.id}" do
         expect(page).to have_content("Quantity: 2")
         click_button "-"
         expect(page).to have_content("Quantity: 1")
@@ -187,16 +187,16 @@ RSpec.describe "cart show page" do
     end
 
     it "has a link to checkout" do
-      @meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
-      @chain = @meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
-      @shifter = @meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
-      visit "/items/#{@chain.id}"
+      visit "/items/#{chain.id}"
       click_button "Add to Cart"
 
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
-      visit "/items/#{@shifter.id}"
+      visit "/items/#{shifter.id}"
       click_button "Add to Cart"
 
       visit "/cart"
