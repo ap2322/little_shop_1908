a15e5b1de72dd76d409fed423285ae62ab31bf52
Zac Isaacson <isaacson.zm@gmail.com>
2019-10-14 16:31:31 -0600

    Adds merchant statistics to merchant show page
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -10,4 +10,16 @@ class Merchant <ApplicationRecord
   def items_in_order?
     !items.joins(:item_orders).empty?
   end
+
+  def count_items
+    items.count(:items)
+  end
+
+  def average_item_price
+    items.average(:price)
+  end
+
+  def customer_cities
+    items.joins(:orders).select('orders.city').distinct.pluck('orders.city')
+  end
 end
