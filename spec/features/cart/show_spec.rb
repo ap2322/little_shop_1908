70da6cfb47d2d9118e7bdd027c3a6739485628d8
Alice Post <alicep2322@gmail.com>
2019-10-15 15:09:38 -0600

    Cart increment flash message fix
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- spec/features/cart/show_spec.rb
+++ spec/features/cart/show_spec.rb
@@ -122,15 +122,15 @@ RSpec.describe "cart show page" do
         click_button "+"
         expect(page).to have_content("Quantity: 2")
       end
-      expect(current_path).to eq('/cart')
+      expect(page).to_not have_content("Only 5 Chains in stock")
 
       within "#item-#{@shifter.id}" do
         expect(page).to have_content("Quantity: 2")
         click_button "+"
         expect(page).to have_content("Quantity: 2")
       end
+
       expect(page).to have_content("Only 2 Shimano Shifters in stock")
-      expect(current_path).to eq('/cart')
     end
 
     it "can decrement the count of an individual item according to inventory" do
