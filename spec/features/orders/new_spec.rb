b0d3f606d642c0ee6ee7bf85a5a75f3e99424c1c
Alice Post <alicep2322@gmail.com>
2019-10-15 10:50:18 -0600

    Clear cart when order is created
    Co-authored-by: Rachel Lew <48839191+rlew421@users.noreply.github.com>
    Co-authored-by: Zac Isaacson <50255174+zacisaacson@users.noreply.github.com>

--- spec/features/orders/new_spec.rb
+++ spec/features/orders/new_spec.rb
@@ -126,8 +126,38 @@ RSpec.describe "On the Checkout Page (aka New Order page)" do
 
       expect(page).to have_content("Please fill in all fields to complete your order")
       expect(page).to have_button("Create Order")
+    end
+  end
 
+  describe "When I make an order" do
+    it "empties our cart" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
 
+      visit "/items/#{chain.id}"
+      click_button "Add to Cart"
+      visit "/items/#{shifter.id}"
+      click_button "Add to Cart"
+      visit "/items/#{shifter.id}"
+      click_button "Add to Cart"
+
+      visit '/orders/new'
+
+      fill_in :name, with: "Bob G"
+      fill_in :address, with: "5555 Neverwhere Ln"
+      fill_in :city, with: "Loveland"
+      fill_in :state, with: "CO"
+      fill_in :zip, with: "80004"
+
+      click_button "Create Order"
+
+      order = Order.last
+
+      within "#cart" do
+        expect(page).to have_content("0")
+      end
     end
   end
 end
