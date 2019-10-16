4773f2b6d0f59a8952af9d6392a2e4bdc0782b84
Rachel Lew <rlew07@gmail.com>
2019-10-16 12:41:48 -0600

    Adds feature test, model test, and method to calculate top three items for merchant
    Co-authored-by: Alice Post <26877629+ap2322@users.noreply.github.com>
    Co-authored-by: Zac Isaacson  <50255174+zacisaacson@users.noreply.github.com>

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -22,4 +22,16 @@ class Merchant <ApplicationRecord
   def customer_cities
     items.joins(:orders).select('orders.city').distinct.pluck('orders.city')
   end
+
+  def top_items
+    # All in ruby, need to find Active Record way to do the same thing!
+    avg_ratings = {}
+    items.each do |item|
+      key = item.id
+      avg_ratings[key] = item.average_rating
+    end
+    sorted = avg_ratings.sort_by{|item_id, rating| rating}.reverse
+    top_3 = sorted.map {|item_id, rating| items.find(item_id)}
+    top_3[0..2]
+  end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -57,5 +57,46 @@ RSpec.describe 'merchant show page', type: :feature do
       expect(page).to have_content("Aurora")
       expect(page).to have_content("Denver")
     end
+
+    it "displays top three highest rated items for that merchant by average rating" do
+      meg = Merchant.create(name: "Meg's Bike Shop", address: '123 Bike Rd.', city: 'Denver', state: 'CO', zip: 80203)
+      tire = meg.items.create(name: "Gatorskins", description: "They'll never pop!", price: 100, image: "https://www.rei.com/media/4e1f5b05-27ef-4267-bb9a-14e35935f218?size=784x588", inventory: 12)
+      chain = meg.items.create(name: "Chain", description: "It'll never break!", price: 50, image: "https://www.rei.com/media/b61d1379-ec0e-4760-9247-57ef971af0ad?size=784x588", inventory: 5)
+      shifter = meg.items.create(name: "Shimano Shifters", description: "It'll always shift!", active?: false, price: 180, image: "https://images-na.ssl-images-amazon.com/images/I/4142WWbN64L._SX466_.jpg", inventory: 2)
+      helmet = meg.items.create(name: "Helmet", description: "It'll protect your noggin!", price: 40, image: "https://images-na.ssl-images-amazon.com/images/I/71kr1GfrR1L._SX466_.jpg", inventory: 5)
+
+      tire_review_1 = tire.reviews.create(title: "Went flat", content: "super smelly", rating: 1)
+      tire_review_2 = tire.reviews.create(title: "The road shredded these tires", content: "goddawful", rating: 1)
+      tire_review_3 = tire.reviews.create(title: "Would not recommend", content: "Absolutely terrible", rating: 1)
+
+      chain_review_1 = chain.reviews.create(title: "This stunk", content: "super smelly", rating: 1)
+      chain_review_2 = chain.reviews.create(title: "This blew my mind", content: "goddawful", rating: 1)
+      chain_review_3 = chain.reviews.create(title: "This was great", content: "It worked just as described", rating: 5)
+
+      shifter_review_1 = shifter.reviews.create(title: "Not bad", content: "This was ok", rating: 3)
+      shifter_review_2 = shifter.reviews.create(title: "It shifted as described", content: "No problems so far", rating: 4)
+      shifter_review_3 = shifter.reviews.create(title: "Pretty good", content: "I'd recommend these to a friend", rating: 4)
+
+      helmet_review_1 = helmet.reviews.create(title: "Great", content: "Protects my brain", rating: 4)
+      helmet_review_2 = helmet.reviews.create(title: "Amazing", content: "Fell off my bike and I was fine", rating: 5)
+      helmet_review_3 = helmet.reviews.create(title: "Wow!", content: "Like a shield for head", rating: 5)
+
+      visit "/merchants/#{meg.id}"
+
+      expect(page).to have_content(helmet.name)
+      # expect(page).to have_content(helmet.image)
+      expect(page).to have_content("Average Rating: 4.7")
+      expect(page).to have_content(shifter.name)
+      # expect(page).to have_content(shifter.image)
+      expect(page).to have_content("Average Rating: 3.7")
+      expect(page).to have_content(chain.name)
+      # expect(page).to have_content(chain.image)
+      expect(page).to have_content("Average Rating: 2.3")
+      expect(page).to_not have_content(tire.name)
+    end
   end
 end
+
+# As a visitor,
+# When I visit an merchant's show page
+# I see the top 3 highest rated items for that merchant (by average rating)
